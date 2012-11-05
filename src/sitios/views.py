ccfd967ad6dbd9868b794f6d563f5d174eeb5038
luis andres <luis@luis-desktop.(none)>
2012-11-04 23:45:02 -0500

    funcion para encontrar los puntos cercanos a un punto dado

--- src/sitios/views.py
+++ src/sitios/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+
+def findNearSites(latitud, longitud, tipoSitio):
+
+    latitudini = float(latitud)
+    longitudini = float(longitud)
+
+
+    stringSQL = "SELECT osm_id, amenity, name, way"
+    stringSQL = stringSQL + " " + "FROM planet_osm_point" 
+    stringSQL = stringSQL + " " + "ST_Within(way, ST_Buffer(ST_Transform(ST_GeomFromText('POINT(" + str(longitudini) 
+    stringSQL = stringSQL + " " + " " + str(latitudini) + ")', 4326), 900913), 500, 'quad_segs=4')"
+    stringSQL = stringSQL + " " + " amenity = '" + tipoSitio + "'"
+
+    cursor = connection.cursor()
+    cursor.execute(stringSQL)
+
